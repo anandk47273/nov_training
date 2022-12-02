6e3707c5fee88cfe0d983c5044fe2785abb5d77e
anand <anandk47273@gmail.com>
2022-12-02 11:45:47 +0530

    commited some new doctypes

---
+++ erp_training/erp_training/doctype/property_doc/property_doc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, anand and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Property doc', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ erp_training/erp_training/doctype/training/training.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, anand and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Training', {
+	// refresh: function(frm) {
+
+	// }
+});
