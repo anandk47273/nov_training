725f14ca240357d7f677e4201b34a04663e80a5a
anand <anandk47273@gmail.com>
2022-12-08 15:42:11 +0530

    deleted some custom doctypes

--- erp_training/erp_training/doctype/server_side_script/server_side_script.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, anand and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Server side script', {
-	// refresh: function(frm) {
-
-	// }
-});
